3587115bba2050b497a00a8cf371b505d0e3a5db
Christopher Bohl <chrisrbohl@gmail.com>
2015-04-23 20:59:09 -0500

    Fake equation working with basic css

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,21 @@
 get '/' do
   # Look in app/views/index.erb
-  puts "Params"
-  puts params
   @cars = Car.all
   erb :index
+end
+
+post '/cars' do
+  puts "PARAMS MAKE"
+  puts params
+  car = Car.find_by(make: params[:make])
+  years_driving = params[:years_driving].to_i
+  miles_per_year = params[:miles_per_year].to_i
+  erb :"_car", layout: false, locals: {car: car, years_driving: years_driving, miles_per_year: miles_per_year}
 end
 
 post '/' do
   puts "Params"
   puts params
 end
+
+
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $("#car_form").on("submit", function(event){
+    event.preventDefault();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+
+    console.log($(this).serialize())
+
+    request = $.ajax({
+                url: "/cars",
+                method: "post",
+                data: $(this).serialize()
+    })
+
+
+
+    request.done(function(response){
+      console.log(response)
+      $("#car_1").remove()
+      $("#car_1_position").append(response)
+    })
+
+
+
+
+
+
+  })
 });
